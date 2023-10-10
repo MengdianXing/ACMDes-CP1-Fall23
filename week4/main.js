c1dc1a3353fd8897b31558d4c47bde71dc76649b
MengdianXing <144857261+MengdianXing@users.noreply.github.com>
2023-10-09 19:57:14 -0500

    Week 4

---
+++ week4/main.js
@@ -0,0 +1,9 @@
+LEt switchImage = function(imgID){
+    console.log("fired")
+    document.getElementById("largeImg)"). src.=".ipg"
+
+}
+document.getElementById("babymonkey").addEventListener("click",switchImage, "babaymonkey")
+document.getElementById("mirrormonkey").addEventListener("click",switchImage)
+document.getElementById("zhumonkey").addEventListener("click",switchImage)
+document.getElementById("blackmonkey").addEventListener("click",switchImage)
